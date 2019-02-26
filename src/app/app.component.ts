bd59a166a0361b0a4ddf7f9cbd3cb804d0e44818
Steve Avon <savon@vonage.net>
2019-02-26 16:56:16 -0500

    finished template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,37 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    gender: '',
+    secret: '',
+    answer: ''
+  }
+  submitted = false;
 
   
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // set value you pass an exact copy of form with values set
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'mail'
+    // });
+
+    //patch value can just set 1 value without ruining rest of form
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -24,6 +51,13 @@ export class AppComponent {
 
 
   onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.gender = this.signupForm.value.gender;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 }
